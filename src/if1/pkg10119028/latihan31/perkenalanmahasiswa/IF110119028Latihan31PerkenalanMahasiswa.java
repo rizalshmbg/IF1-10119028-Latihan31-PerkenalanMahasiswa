4b7e49bc18a1726abed0c8b6d1860d74c2346531
Rizal Sihombing <66808677+rizalshmbg@users.noreply.github.com>
2020-11-06 09:02:38 +0700

    Add files via upload

---
+++ src/if1/pkg10119028/latihan31/perkenalanmahasiswa/IF110119028Latihan31PerkenalanMahasiswa.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119028.latihan31.perkenalanmahasiswa;
+
+/**
+ *
+ * @author
+ * NAMA     : Rizal Sihombing
+ * KELAS    : IF1
+ * NIM      : 10119028
+ * Deskripsi Program : Program ini berisi program untuk menampilkan identitas
+ *                     Mahasiswa baru dengan konsep OOP 
+ */
+public class IF110119028Latihan31PerkenalanMahasiswa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Mahasiswa mahasiswa1 = new Mahasiswa();
+        mahasiswa1.nim = "10119010";
+        mahasiswa1.nama = "Maulana Imam Malik";
+        
+        Mahasiswa mahasiswa2 = new Mahasiswa();
+        mahasiswa2.nim = "10110269";
+        mahasiswa2.nama = "Rizki Adam Kurniawan";
+        
+        Mahasiswa mahasiswa3 = new Mahasiswa();
+        mahasiswa3.nim = "10110270";
+        mahasiswa3.nama = "Indra Tiola";
+        
+        Mahasiswa mahasiswa4 = new Mahasiswa();
+        mahasiswa4.nim = "101190269";
+        mahasiswa4.nama = "Muhammad Nur Awaludin";
+        
+        mahasiswa1.perkenalanDiri();
+        mahasiswa2.perkenalanDiri();
+        mahasiswa3.perkenalanDiri();
+        mahasiswa4.perkenalanDiri();
+    }
+    
+}
---
+++ src/if1/pkg10119028/latihan31/perkenalanmahasiswa/Mahasiswa.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119028.latihan31.perkenalanmahasiswa;
+
+/**
+ *
+ * @author
+ * NAMA     : Rizal Sihombing
+ * KELAS    : IF1
+ * NIM      : 10119028
+ * Deskripsi Program : Program ini berisi program untuk menampilkan identitas
+ *                     Mahasiswa baru dengan konsep OOP 
+ */
+public class Mahasiswa {
+    public String nim;
+    public String nama;
+    
+    public void perkenalanDiri() {
+        System.out.println("Hello Everyone");
+        System.out.println("My NIM is ".concat(nim));
+        System.out.println("My Name is ".concat(nama).concat("\n"));
+    }
+}
